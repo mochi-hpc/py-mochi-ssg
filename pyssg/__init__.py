7cadab31c596b06782b9706639fbcb9c397e2748
Matthieu Dorier <mdorier@anl.gov>
2018-03-25 17:18:43 +0200

    added copyright info

--- pyssg/__init__.py
+++ pyssg/__init__.py
@@ -1,3 +1,5 @@
+# (C) 2018 The University of Chicago
+# See COPYRIGHT in top-level directory.
 import _pyssg
 import pymargo
 
--- test/server.py
+++ test/server.py
@@ -1,3 +1,5 @@
+# (C) 2018 The University of Chicago
+# See COPYRIGHT in top-level directory.
 from pymargo import MargoInstance
 import pyssg
 
